b540d74ff1d5fa911fbe84e3256d0d15f6d2f2eb
Aksel Allas <allasaksel@gmail.com>
2019-02-28 10:14:21 +0000

    Fixes 1. step xml.

--- gpt.sh
+++ gpt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#/usr/local/snap6/bin/gpt CNN_piloot_preprocessing.xml -c 3G -q 4 -x -J-Xmx12G -J-Xms1G
-/usr/local/snap6/bin/gpt CNN_piloot_preprocessing_2.xml -c 3G -q 4 -x -J-Xmx12G -J-Xms1G
+/usr/local/snap6/bin/gpt CNN_piloot_preprocessing.xml -c 3G -q 4 -x -J-Xmx12G -J-Xms1G
+#/usr/local/snap6/bin/gpt CNN_piloot_preprocessing_3.xml -c 3G -q 4 -x -J-Xmx12G -J-Xms1G
