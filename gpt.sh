40315d539f482af837daee53fa3d9085ac283078
Aksel <allasaksel@gmail.com>
2019-03-14 13:32:20 +0200

    Added NPCI and EVI bands to 1. xml

--- gpt.sh
+++ gpt.sh
@@ -3,24 +3,8 @@ for p in $(ls ../S2_Products); do
 	p3=${p:11:-46}
 	sed -i "s/@@@@/$p/g" CNN_piloot_preprocessing.xml
 	sed -i "s/TTTT/$p3/g" CNN_piloot_preprocessing.xml
-	/home/paku/snap/bin/gpt CNN_piloot_preprocessing.xml -c 1G -q 3 -x -J-Xmx3G -J-Xms1G
+	/home/paku/snap/bin/gpt CNN_piloot_preprocessing.xml -c 1200M -q 3 -x -J-Xmx3G -J-Xms1G
 	sed -i "s/$p/@@@@/g" CNN_piloot_preprocessing.xml
 	sed -i "s/$p3/TTTT/g" CNN_piloot_preprocessing.xml
 	break
 done
-
-#for f in $(ls ../Masks); do
-#	index=$((${#f}-3))
-#	f_extension="${f:$index:3}"
-#	if [ "$f_extension" = "shp" ]; then
-#		f2=${f::-4}
-#		sed -i "s/@@@@/$f/g" CNN_piloot_preprocessing_2.xml
-#		sed -i "s/%%%%/$f2/g" CNN_piloot_preprocessing_2.xml
-#		f3="Mask_${f2}"
-#		sed -i "s/TTTT/$f3/g" CNN_piloot_preprocessing_2.xml
-#		/home/paku/snap/bin/gpt CNN_piloot_preprocessing_2.xml -c 1G -q 3 -x -J-Xmx3G -J-Xms1G
-#		sed -i "s/$f/@@@@/g" CNN_piloot_preprocessing_2.xml
-#		sed -i "s/$f3/TTTT/g" CNN_piloot_preprocessing_2.xml
-#		sed -i "s/$f2/%%%%/g" CNN_piloot_preprocessing_2.xml
-#	fi
-#done
