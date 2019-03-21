9286f4507189a7fa7c845d10dee69f27089ea5f9
Aksel Allas <allasaksel@gmail.com>
2019-03-21 14:44:05 +0000

    Improved mask.sh

--- mask.sh
+++ mask.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
-for f in $(ls ../Masks); do
-        index=$((${#f}-3))
-        f_extension="${f:$index:3}"
-        if [ "$f_extension" = "shp" ]; then
-                f2=${f::-4}
-                sed -i "s/@@@@/$f/g" CNN_piloot_preprocessing_2.xml
-                sed -i "s/%%%%/$f2/g" CNN_piloot_preprocessing_2.xml
-                f3="Mask_${f2}"
-                sed -i "s/TTTT/$f3/g" CNN_piloot_preprocessing_2.xml
-                /home/paku/snap/bin/gpt CNN_piloot_preprocessing_2.xml -c 1G -q 3 -x -J-Xmx3G -J-Xms1G
-                sed -i "s/$f/@@@@/g" CNN_piloot_preprocessing_2.xml
-                sed -i "s/$f3/TTTT/g" CNN_piloot_preprocessing_2.xml
-                sed -i "s/$f2/%%%%/g" CNN_piloot_preprocessing_2.xml
+GPT_PATH=/usr/local/snap6/bin/gpt
+MASK_PATH=/home/rus/Aksel/Andmed/Masks/
+OUTPUT_PATH=/home/rus/Aksel/Andmed/
+INPUT_DIM=/home/rus/Aksel/Andmed/20180520.dim
+
+for mask_file in $(ls ../../Andmed/Masks); do
+        index=$((${#mask_file}-3))
+        mask_file_extension="${mask_file:$index:3}"
+        if [ "${mask_file_extension}" = "shp" ]; then
+                mask=${mask_file::-4}
+		echo $mask
+                prefix_mask="Mask_${mask}"
+                sed "s@{{input_dim}}@${INPUT_DIM}@g; s@{{mask_file}}@${MASK_PATH}${mask_file}@g; s@{{mask}}@${mask}@g; s@{{prefix_mask}}@${OUTPUT_PATH}${prefix_mask}@g" CNN_piloot_preprocessing.in > CNN_piloot_preprocessing_out.xml
+                $GPT_PATH CNN_piloot_preprocessing_out.xml -c 4G -q 3 -x -J-Xmx15G -J-Xms4G
         fi
 done
 
