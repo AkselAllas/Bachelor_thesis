3ce544419c9e105358c43b9585490c964796cc75
Aksel Allas <allasaksel@gmail.com>
2019-05-08 15:44:37 +0300

    Tiny fix, that makes labels preprocessing script use 9x less storage

--- Scripts/label_mask.sh
+++ Scripts/label_mask.sh
@@ -2,15 +2,15 @@
 echo "================ Phase 1 ================="
 
 GPT_PATH=/sar/aksel_snap/snap/bin/gpt
-OUTPUT_PATH=/sar/aksel_snap/
-INPUT_PATH=/home/aksel/Bachelor_thesis/S2_Products/
+OUTPUT_PATH=/sar/aksel/Bachelor_thesis/Scripts/
+INPUT_PATH=/sar/aksel/Bachelor_thesis/S2_Products/
 
 #Generate .dim files, which are needed to make label masks 
 for product in $(ls $INPUT_PATH); do
         index=$((${#product}-4))
         product_file_extension="${product:$index:4}"
         prefix_product=${product:11:-46}
-        #break #Uncomment this to skip Phase 1
+        #break #Uncomment this line to skip Phase 1
         if [ "${product_file_extension}" = "SAFE" ]; then
                 for (( k = 0; k < 2; k++ )); do
                         for (( l = 0; l < 2; l++ )); do
@@ -26,15 +26,15 @@ for product in $(ls $INPUT_PATH); do
 done
 
 echo "================ Phase 2 ================="
-#Make label masks
+#Make label mask tifs.
 #This script throws an error, when there are no fields in the current tile. That means no file will be made for that class/tile combination. We will generate empty files in Phase 3
 
-MASK_PATH=/home/aksel/Bachelor_thesis/Masks/
-OUTPUT_PATH=/sar/aksel_snap/Label_tifs/
+MASK_PATH=/sar/aksel/Bachelor_thesis/Masks/
+OUTPUT_PATH=/sar/aksel/Bachelor_thesis/Label_tifs/
 for (( k = 0; k < 2; k++ )); do
         for (( l = 0; l < 2; l++ )); do
-                #break #Uncomment this to skip Phase 2
-                INPUT_PATH=/sar/aksel_snap/${prefix_product}_${l}-${k}.dim
+                #break #Uncomment this line to skip Phase 2
+                INPUT_PATH=/sar/aksel/Bachelor_thesis/Scripts/${prefix_product}_${l}-${k}.dim
                 for mask_file in $(ls $MASK_PATH); do
                         index=$((${#mask_file}-3))
                         mask_file_extension="${mask_file:$index:3}"
@@ -54,5 +54,5 @@ echo "================ Phase 3 ================="
 #Generate empty files, so that during training, the amount of classes would still be the same for every tile.
 
 for missing_file in `sed -n '/Error/{n;p}' error_tiles.txt`; do
-    touch /sar/aksel_snap/Label_tifs/Mask_${missing_file}.tif
+    touch ${OUTPUT_PATH}Mask_${missing_file}.tif
 done
